dd2c494478bb3aca2fdcc5f44aacad446056bf6c
SBALAMU1 <balan.ssn@gmail.com>
2023-03-10 13:10:25 +0530

    Implemented navigation setup

---
+++ app/src/main/java/com/project/football_module/SingleActivity.kt
@@ -0,0 +1,7 @@
+package com.project.football_module
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SingleActivity : AppCompatActivity(R.layout.activity_main)
