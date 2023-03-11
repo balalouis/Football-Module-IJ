085b06477e93089bcdc33026fd26def45a6d1426
SBALAMU1 <balan.ssn@gmail.com>
2023-03-11 15:25:51 +0530

    Implemented the bottom navigation with navigation component

--- app/src/main/java/com/project/football_module/SingleActivity.kt
+++ app/src/main/java/com/project/football_module/SingleActivity.kt
@@ -1,7 +1,31 @@
 package com.project.football_module
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.project.football_module.databinding.ActivitySingleBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SingleActivity : AppCompatActivity(R.layout.activity_main)
+class SingleActivity : AppCompatActivity(){
+
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivitySingleBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single)
+
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        // Find reference to bottom navigation view
+        val navView: BottomNavigationView = findViewById(R.id.bottom_nav_view)
+        // Hook your navigation controller to bottom navigation view
+        navView.setupWithNavController(navController)
+    }
+
+}
