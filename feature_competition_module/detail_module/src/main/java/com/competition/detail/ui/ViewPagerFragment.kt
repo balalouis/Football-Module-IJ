b193779d9f750ba227e9217c44f394673cf57f25
SBALAMU1 <balan.ssn@gmail.com>
2023-03-13 13:30:57 +0530

    Added viewpager fragments

--- feature_competition_module/detail_module/src/main/java/com/competition/detail/ui/ViewPagerFragment.kt
+++ feature_competition_module/detail_module/src/main/java/com/competition/detail/ui/ViewPagerFragment.kt
@@ -6,10 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.competition.detail.databinding.FragmentViewPagerBinding
+import com.project.detail.adapter.ViewPageAdapter
 
 class ViewPagerFragment : Fragment() {
 
     private lateinit var binding: FragmentViewPagerBinding
+    private var competitionId: Long = 0L
+    private lateinit var name: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,4 +21,31 @@ class ViewPagerFragment : Fragment() {
         binding = FragmentViewPagerBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
+
+    private fun initAdapter(){
+
+        val viewPageAdapter = ViewPageAdapter(
+            childFragmentManager
+        )
+        viewPageAdapter.addFragment(TableFragment().apply {
+            arguments = Bundle().apply {
+                putLong("id", competitionId)
+            }
+        }, "Table")
+        viewPageAdapter.addFragment(FixturesFragment().apply {
+            arguments = Bundle().apply {
+                putLong("id", competitionId)
+            }
+        }, "Fixtures")
+        viewPageAdapter.addFragment(TeamFragment().apply {
+            arguments = Bundle().apply {
+                putLong("id", competitionId)
+            }
+        }, "Team")
+
+        // Set up the ViewPager with the sections adapter.
+        binding.container.adapter = viewPageAdapter
+        binding.tabs.setupWithViewPager(binding.container)
+    }
+
 }
