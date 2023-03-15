c35804f3751a764d28de85c2e83d54ee5e0735f1
SBALAMU1 <balan.ssn@gmail.com>
2023-03-15 18:17:45 +0530

    Added Picasso and set the image resource

--- feature_competition_module/detail_module/src/main/java/com/competition/detail/adapter/TeamListAdapter.kt
+++ feature_competition_module/detail_module/src/main/java/com/competition/detail/adapter/TeamListAdapter.kt
@@ -3,8 +3,10 @@ package com.competition.detail.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.competition.detail.R
 import com.competition.detail.databinding.TeamListRowBinding
 import com.project.room.model.RoomTeam
+import com.squareup.picasso.Picasso
 
 class TeamListAdapter(private var roomTeamList: List<RoomTeam>) :
     RecyclerView.Adapter<TeamListAdapter.TeamListViewHolder>() {
@@ -20,6 +22,16 @@ class TeamListAdapter(private var roomTeamList: List<RoomTeam>) :
 
     override fun onBindViewHolder(holder: TeamListViewHolder, position: Int) {
         val roomTeam=roomTeamList[position]
+        holder.binding.clubName.text = roomTeam.name
+        setImageResource(holder,roomTeam)
+    }
+
+    private fun setImageResource(holder: TeamListViewHolder, roomTeam: RoomTeam){
+        Picasso.get()
+            .load(roomTeam.crest).fit().centerCrop()
+            .placeholder(R.drawable.ic_baseline_account_circle_24)
+            .error(R.drawable.ic_baseline_account_circle_24)
+            .into(holder.binding.clubLogo)
     }
 
     override fun getItemCount(): Int {
