87b426f126b09e22f614c37e6e12bf972ff52b57
SBALAMU1 <balan.ssn@gmail.com>
2023-03-15 17:03:42 +0530

    Created team model for room databases

--- room/src/main/java/com/project/room/FootballDatabase.kt
+++ room/src/main/java/com/project/room/FootballDatabase.kt
@@ -5,11 +5,12 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.project.room.model.*
 
-@Database(entities = [Match::class,CompetitionsUiState::class, Table::class], version = 1)
+@Database(entities = [Match::class,CompetitionsUiState::class, Table::class, RoomTeam::class], version = 1)
 @TypeConverters(MatchTypeConverter::class, AwayTeamTypeConverter::class,
     HomeTeamTypeConverter::class, ScoreTypeConverter::class,
     FullTimeTypeConverter::class, HalfTimeTypeConverter::class,
-    CurrentSeasonTypeConverter::class, TableListConverter::class,TeamTypeConverter::class, TableTypeConverter::class)
+    CurrentSeasonTypeConverter::class, TableListConverter::class,TeamTypeConverter::class, TableTypeConverter::class,
+    RoomTeamTypeConverter::class)
 abstract class FootballDatabase : RoomDatabase() {
     abstract fun footballDao(): FootballDao
 }
--- room/src/main/java/com/project/room/model/TeamEntities.kt
+++ room/src/main/java/com/project/room/model/TeamEntities.kt
@@ -2,6 +2,9 @@ package com.project.room.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 @Entity(tableName = "team_list")
 data class RoomTeam(
@@ -9,3 +12,18 @@ data class RoomTeam(
     val name: String,
     val crest: String,
 )
+
+class RoomTeamTypeConverter {
+    private val gson = Gson()
+
+    @TypeConverter
+    fun roomTeamToString(roomTeam: RoomTeam): String {
+        return gson.toJson(roomTeam)
+    }
+
+    @TypeConverter
+    fun stringToRoomTeam(roomTeamString: String): RoomTeam {
+        val objectType = object : TypeToken<RoomTeam>() {}.type
+        return gson.fromJson(roomTeamString, objectType)
+    }
+}
