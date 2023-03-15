ece81494411ced3a83167ad14982f3e7b384c57c
SBALAMU1 <balan.ssn@gmail.com>
2023-03-15 18:19:39 +0530

    Added Picasso and set the image resource table

--- feature_competition_module/detail_module/src/main/java/com/competition/detail/adapter/TableAdapter.kt
+++ feature_competition_module/detail_module/src/main/java/com/competition/detail/adapter/TableAdapter.kt
@@ -4,8 +4,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.competition.detail.R
 import com.competition.detail.databinding.TableListRowBinding
+import com.project.room.model.RoomTeam
 import com.project.room.model.Table
+import com.squareup.picasso.Picasso
 
 class TableListAdapter(private var tableList: List<Table>) :
     RecyclerView.Adapter<TableListAdapter.TableListViewHolder>() {
@@ -26,6 +29,15 @@ class TableListAdapter(private var tableList: List<Table>) :
         holder.binding.score1.text = table.playedGames.toString()
         holder.binding.score2.text = table.goalDifference.toString()
         holder.binding.score3.text = table.points.toString()
+        setImageResource(holder,table)
+    }
+
+    private fun setImageResource(holder: TableListViewHolder, table: Table){
+        Picasso.get()
+            .load(table.team.crest).fit().centerCrop()
+            .placeholder(R.drawable.ic_baseline_account_circle_24)
+            .error(R.drawable.ic_baseline_account_circle_24)
+            .into(holder.binding.clubLogo)
     }
 
     override fun getItemCount(): Int {
