d2543b5820adb1efef709e9f98a44dc8b75a2979
SBALAMU1 <balan.ssn@gmail.com>
2023-03-15 17:46:46 +0530

    Added hilt module

--- feature_competition_module/data/src/main/java/com/competition/detail/data/hilt/HiltDetailModule.kt
+++ feature_competition_module/data/src/main/java/com/competition/detail/data/hilt/HiltDetailModule.kt
@@ -2,9 +2,14 @@ package com.competition.detail.data.hilt
 
 import com.competition.detail.data.datasource.table.RemoteDataSourceGetTable
 import com.competition.detail.data.datasource.table.RemoteDataSourceGetTableImpl
+import com.competition.detail.data.datasource.team.RemoteDataSourceGetTeam
+import com.competition.detail.data.datasource.team.RemoteDataSourceGetTeamImpl
 import com.competition.detail.data.repo.TableRepositoryImpl
+import com.competition.detail.data.repo.TeamRepositoryImpl
 import com.competition.detail.domain.repo.TableRepository
+import com.competition.detail.domain.repo.TeamRepository
 import com.competition.detail.domain.usecases.GetAllTableUseCases
+import com.competition.detail.domain.usecases.GetAllTeamUseCases
 import com.project.network.hilt.api.ApiService
 import com.project.room.FootballDao
 import dagger.Module
@@ -29,6 +34,21 @@ object HiltDetailModule {
     @Provides
     fun provideTableRemoteDataSource(footballDao: FootballDao, apiService: ApiService):RemoteDataSourceGetTable{
         return RemoteDataSourceGetTableImpl(footballDao, apiService)
+    }
+
+    @Provides
+    fun provideGetAllTeamUseCases(teamRepository: TeamRepository): GetAllTeamUseCases {
+        return GetAllTeamUseCases(teamRepository)
+    }
+
+    @Provides
+    fun provideGetTeamRepository(remoteDataSourceGetTeam: RemoteDataSourceGetTeam): TeamRepository{
+        return TeamRepositoryImpl(remoteDataSourceGetTeam)
+    }
+
+    @Provides
+    fun provideTeamRemoteDataSource(footballDao: FootballDao, apiService: ApiService):RemoteDataSourceGetTeam{
+        return RemoteDataSourceGetTeamImpl(footballDao, apiService)
     }
 
 }
